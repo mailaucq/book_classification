fa88ce987b12cf881ddfaeaf752e82eebc8f4bae
mailau <lvcruzq@gmail.com>
2021-05-26 15:04:07 -0300

    changes for show plot when is adding edges with word embedding

--- plot_graphics_variability.py
+++ plot_graphics_variability.py
@@ -3,15 +3,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-dimensions = range(2)
-sizes = [300, 600, 900, 1200, 1500, 1800, 2100]
+dimensions = range(21)
+sizes = [300, 600, 900, 1500, 1800, 2100]
 datasets = ["13authors"]#["stanisz", "brown", "vanessa"]
 measures = ["dgr_n", "btw", "cc", "sp", "sp_std", "accs_h2", "accs_h3"]
 measures_name = ["Average Neighbor Degree $<k_n>$", "Average Betweeness $<B>$", "Average Clustering Coeficient $<C>$", "Average Shortest Path Lenght $<l>$", r"Standard Desviation Shortest Path Lenght $\sigma{l}$", r"Average Accessibility $<\alpha^{h=2}$>", r"Average Accessibility $<\alpha^{h=3}$>"] #["Average Degree $<k_n>$", "Average PageRank $<PR>$", "Average Betweeness $<B>$", "Average Clustering Coeficient $<C>$", "Average Shortest Path Lenght $<l>$", "Average Symmetry Backbone $<Sb_{h=2}>$", "Average Symmetry Merge $<Sm_{h=2}>$", "Average Symmetry Backbone $<Sb_{h=3}>$", "Average Symmetry Merge $<Sm_{h=3}$>", r"Average Accessibility $<\alpha^{h=2}$>", r"Average Accessibility $<\alpha^{h=3}$>"]
 
 
 for measure, measure_name in zip(measures, measures_name):
-	for dim in dimensions:
+	fig, ax = plt.subplots()
+	barwidth = 0	
+	for dim in range(0,21,5):
 		means = []
 		errors = []
 		for size in sizes:
@@ -28,24 +30,25 @@ for measure, measure_name in zip(measures, measures_name):
 			errors.append(std)
 
 		materials = sizes
-		x_pos = np.arange(len(materials))
+		r1 = np.arange(len(materials))
+		x_pos = [x + barwidth for x in r1]
+		ax.bar(x_pos, means, yerr=errors, width=0.25, align='center', alpha=0.5, ecolor='black', capsize=10, label=str(dim) + "%")
+		barwidth += 0.15
+	ax.set_ylabel('Coefficient of Variation')
+	ax.set_xlabel(str(dim))
+	ax.set_xticks(x_pos)
+	ax.set_xticklabels(materials)
+	ax.set_title(measure_name)
+	ax.yaxis.grid(True)
 
-		fig, ax = plt.subplots()
-		ax.bar(x_pos, means, yerr=errors, align='center', alpha=0.5, ecolor='black', capsize=10)
-		ax.set_ylabel('Coefficient of Variation')
-		ax.set_xlabel(str(dim))
-		ax.set_xticks(x_pos)
-		ax.set_xticklabels(materials)
-		ax.set_title(measure_name)
-		ax.yaxis.grid(True)
-
-		# Save the figure and show
-		plt.tight_layout()
-		plt.savefig(measure + str(dim) +'.png')
-		plt.show()
+	# Save the figure and show
+	plt.legend()
+	plt.tight_layout()
+	plt.savefig(measure + str(dim) +'.png')
+	plt.show()
 	
 
-
+'''
 
 for measure, measure_name in zip(measures, measures_name):
 	
@@ -84,4 +87,4 @@ for measure, measure_name in zip(measures, measures_name):
 		plt.show()
 		
 	
-	
+'''	
